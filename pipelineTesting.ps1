ab29116e4fcdd1022e2fb8a5b914625cbfdc3310
Dylan Bright <dylanbright@Dylans-MacBook-Pro.local>
2019-10-26 09:50:06 -0400

    param the storage key

--- pipelineTesting.ps1
+++ pipelineTesting.ps1
@@ -1,6 +1,7 @@
 #Example use of the pslogger class.
 
 using module "./dcb-pslogger/drop/pslogger/pslogger.psm1"
+param ($azStorageKey)
 #using module @{ModuleName="$(Build.ArtifactStagingDirectory)/pslogger/pslogger.psm1";ModuleVersion="0.0.2"}
 $pslogname = 'testLogging'
 $saveMode ='append' #add enum for this.  Options Append,aggregate
@@ -12,7 +13,7 @@ $storageAccountName = 'dcbscriptsstor001'
 #$storageAccountResourceGroupName  ='dcb-scripts-rg'
 $storageContainerName ='logs'
 #$storageAccountkey = (Get-AzStorageAccountKey -ResourceGroupName $storageAccountResourceGroupName -Name $storageAccountName).value[0]
-$storageAccountkey = $(azStorageKey)
+$storageAccountkey = $azStorageKey
 $mylogger = [pslogger]::new($pslogname,$saveMode,$displayMessages,$outputpath,$outputFilePrefix,$ToAzure,$storageAccountName,$storageContainerName,$storageAccountkey)
 $mylogger.addMessage('mysource','test message')
 $mylogger.addMessage('newsource','mymessage')
